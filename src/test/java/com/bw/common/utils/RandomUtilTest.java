64187d8a63e58b2bb3f1d1898892573656ac2cb3
lbw <2431540589@qq.com>
2020-02-28 21:34:01 +0800

    提交

---
+++ src/test/java/com/bw/common/utils/RandomUtilTest.java
@@ -0,0 +1,35 @@
+package com.bw.common.utils;
+
+import org.junit.Test;
+/**
+ * 
+ * @ClassName: RandomUtilTest 
+ * @Description: TODO
+ * @author: 刘战威
+ * @date: 2020年2月28日 下午9:15:31
+ */
+public class RandomUtilTest {
+
+	@Test
+	public void testRandom() {
+		int i = RandomUtil.random(1,3);
+		System.out.println(i);
+		
+	}
+
+	@Test
+	public void testSubRandom() {
+		
+	}
+
+	@Test
+	public void testRandomCharacter() {
+		
+	}
+
+	@Test
+	public void testRandomString() {
+		
+	}
+
+}
